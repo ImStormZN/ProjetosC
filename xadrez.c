e6ae0e9a2b5f44ed3557629555d50d0cbf283419
ImStormZN <103915300+ImStormZN@users.noreply.github.com>
2025-09-18 02:16:27 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,60 +1,71 @@
 #include <stdio.h>
 
+void movimentoTorre(int n) {
+    if(n > 0) {
+        printf("Direita\n");
+        movimentoTorre(n - 1);
+    }
+}
+
+void movimentoBispo(int n) {
+    if(n > 0) {
+        printf("Cima\n");
+        printf("Direita\n");
+        movimentoBispo(n - 1);
+    }
+}
+
+void movimentoRainha(int n) {
+    if(n > 0) {
+        printf("Esquerda\n");
+        movimentoRainha(n - 1);
+    }
+}
+
+void movimentoCavalo(int n, int t) {
+    if(n > 0) {
+        printf("Baixo\n");
+        movimentoCavalo(n - 1,t);
+    }
+
+    if(n == 0) {
+        if(t > 0) {
+            printf("Esquerda\n");
+            movimentoCavalo(n,t - 1);
+        }
+        
+    }
+}
+
+
 int main () {
+
 
     // Movimento Torre
     printf("======== Movimento Torre ============\n\n");
-
-    for (int i = 1; i <= 5; i++) {
-        
-        printf("Direita\n");
-
-    }
+    movimentoTorre(5);
 
     printf("\n");
 
     // Movimento Bispo
 
     printf("======== Movimento Bispo ============\n\n");
-
-    int c = 0;
-    while (c < 5) {
-        printf("Cima\n");
-        printf("Direita\n");
-        c++;
-    }
+    movimentoBispo(5);
 
     printf("\n");
 
     // Movimento Rainha
 
     printf("======== Movimento Rainha ============\n\n");
-    
-    int rC = 0;
-    do {
-        printf("Esquerda\n");
-        rC++;
-    } while (rC < 8);
+    movimentoRainha(8);
 
     printf("\n");
 
     // Movimento Cavalo
 
     printf("======== Movimento Cavalo ============\n\n");
+    movimentoCavalo(2,1);
 
-    for(int i = 1; i <= 2; i++) {
-
-        printf("Baixo\n");
-
-        if(i == 2) {
-            int c = 0;
-            while(c < 1) {
-                printf("Esquerda\n");
-                c++;
-            }
-        }
-        
-    }
-
+    return 0;
 
 }
